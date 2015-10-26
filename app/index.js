399eecec0c64599f801118d2e94eb42dfc105dad
Brendon Soto <brendon.soto@gmail.com>
2015-10-26 16:46:28 -0400

    Set up prompting system to allow the user to choose between a Banner, Email, or PHP template. Downloads the necessary Node packages upon selection.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,28 @@
 var generators = require('yeoman-generator');
 
+var // Arrays of different dependencies to install
+banner = [], // Banner dependencies
+composer = false, // For Slim
+email = [ // Email Dependencies
+  'gulp-connect', 
+  'gulp-inline-css', 
+  'gulp-litmus', 
+  'gulp-w3cjs'
+],
+php = [ // PHP site dependencies
+  'gulp-autoprefixer', 
+  'browser-sync', 
+  'gulp-concat', 
+  'gulp-connect-php', 
+  'gulp-jshint', 
+  'gulp-minify-css', 
+  'gulp-rename', 
+  'gulp-uglify', 
+  'gulp-watch'
+],
+toInstall = [];
+
+
 module.exports = generators.Base.extend({
 
   constructor: function() {
@@ -10,22 +33,44 @@ module.exports = generators.Base.extend({
   prompting: function() {
     var done = this.async();
     this.prompt({
-      type: 'input',
-      name: 'name',
-      message: 'Your project name',
-      default: this.appname // Default to current folder name
+      choices: [ 'Banner', 'Email', 'PHP site'],
+      type: 'list',
+      name: 'template',
+      message: 'Greetings Dev. What sort of template would you be needing for this project?',
+      default: 'PHP site'
     }, function (answers) {
-      this.log(answers.name);
-      this.log(answers);
+
+      switch (answers.template) {
+        case 'Banner':
+          this.log('Banner template coming soon');
+          break;
+        case 'Email':
+          toInstall = email.slice(0);
+          break;
+        case 'PHP site':
+          toInstall = php.slice(0);
+          composer = true;
+          break;
+        default:
+          break;
+      }
       done();
     }.bind(this));
   },
 
   // Installs
   install: function() {
-    this.log('Installing bootstrap');
-    this.bowerInstall(['bootstrap'], { 'save': true });
-    this.log('Installing Slim');
-    this.spawnCommand('composer', ['install']);
+    this.log('Installing dependencies');
+    this.npmInstall(toInstall, { 'save': true });
+    this.installDependencies();
+    if (composer) {
+      this.log('Installing Slim');
+      this.spawnCommand('composer', ['install']);
+    }
+  },
+
+  paths: function() {
+    this.destinationRoot('./build');
   }
+
 });
